1f59b3384253f18cfbe21c84ea400413345d6c93
Noor AlHadidi <noor.ahmed.alhadidi@gmail.com>
2026-02-23 15:03:31 +0200

    initalised server.ts

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import { createServer } from 'http';
+import { env } from '@/utils';
+import { logger } from '@/utils/logger.util';
+import app from '@/app';
+import { databaseClient } from '@/database';
+
+async function main() {
+    const server = createServer(app);
+
+    // initialize database connection
+    await databaseClient.connect();
+
+    const { port } = env;
+
+    server.listen(port, () => {
+        logger.debug(`Server is running on port ${port}.⬆️ `);
+    });
+}
+
+main();
