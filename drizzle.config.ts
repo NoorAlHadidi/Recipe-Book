5c6471ee6fe3d211ed2adc155e1ec2350349c785
Noor AlHadidi <noor.ahmed.alhadidi@gmail.com>
2026-02-26 16:29:33 +0200

    added user and refresh token tables schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,11 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default defineConfig({
-    schema: "./src/database/schema.ts",
+    schema: "./src/database/schema/*.schema.ts",
     out: "./drizzle/migrations", // directory to store generated migration files
     dialect: "postgresql",
     dbCredentials: {
-        url: process.env.DATABASE_URL!,
+        url: process.env.DB_URL!,
     },
     strict: true, // security precaution for migrations (prevent major changes without confirmation)
     verbose: true, // to log generated SQL queries (tell us what will change when migrating)
---
+++ src/database/schema/users.schema.ts
@@ -0,0 +1,22 @@
+import { pgTable, serial, varchar, pgEnum, timestamp, boolean, integer } from "drizzle-orm/pg-core";
+
+export const moodEnum = pgEnum('role', ['user', 'admin']);
+
+export const usersTable = pgTable("users", {
+  userId: serial("user_id").primaryKey(),
+  firstName: varchar("first_name", { length: 100 }).notNull(),
+  lastName: varchar("last_name", { length: 100 }).notNull(),
+  email: varchar("email").notNull().unique(),
+  password: varchar("password").notNull(),
+  role: moodEnum("role").notNull().default('user'),
+  isActive: boolean("active").notNull().default(true),
+  createdAt: timestamp("created_at", { mode: "date" }).defaultNow(),
+});
+
+export const refreshTokensTable = pgTable("refresh_tokens", {
+  id: serial("id").primaryKey(),
+  userId: integer("user_id").notNull().references(() => usersTable.userId, { onDelete: "cascade" }),
+  token: varchar("token").notNull(),
+  expiresAt: timestamp("expires_at", { mode: "date" }).notNull(),
+});
+
