b6c6e4a49a0d3320ff9994bf6076ac2d8b588cb0
Noor AlHadidi <noor.ahmed.alhadidi@gmail.com>
2026-03-12 01:20:53 +0200

    adding case insensitive checking

--- src/categories/categories.schema.ts
+++ src/categories/categories.schema.ts
@@ -3,8 +3,10 @@ import z from "zod";
 export const addCategorySchema = z.object({
   name: z
     .string()
+    .trim()
     .min(1, "Category name is required.")
-    .max(100, "Category name must be at most 100 characters long."),
+    .max(100, "Category name must be at most 100 characters long.")
+    .transform((category) => category.toLowerCase()),
   description: z.string().optional(),
 });
 
