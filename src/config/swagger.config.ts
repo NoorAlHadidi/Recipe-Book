d06f4ee1450917e990444bfd383a52906a145a48
Noor AlHadidi <noor.ahmed.alhadidi@gmail.com>
2026-03-16 21:17:38 +0200

    modified swagger route comment path

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -27,12 +27,7 @@ const swaggerDefinition = {
 
 const swaggerOptions: swaggerJsdoc.Options = {
   definition: swaggerDefinition,
-  apis: [
-    "src/auth/**/*.ts",
-    "src/admins/**/*.ts",
-    "src/categories/**/*.ts",
-    "src/routes/**/*.ts",
-  ], // where swagger looks for route comments
+  apis: ["src/**/*.route.ts"], // where swagger looks for route comments
 };
 
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
